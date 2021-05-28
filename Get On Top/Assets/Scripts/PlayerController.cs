d045512dc5216487ed01acb3ce1b5344aa66863f
TomDotScott <tom.scott45456@gmail.com>
2021-05-28 14:36:12 +0100

    Better player controls
    The player can double jump and accelerate downwards

--- Get On Top/Assets/Scripts/PlayerController.cs
+++ Get On Top/Assets/Scripts/PlayerController.cs
@@ -7,19 +7,74 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
 
+    private Rigidbody2D rb;
+
+    private bool onGround = false;
+    private bool canDoubleJump = false;
+
+    private float doubleJumpTimer;
+    [SerializeField] private float timeBetweenJumps;
+
+    [SerializeField] private float gravityScale;
+    [SerializeField] private float fastFallGravityScale;
+
     void Start()
     {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.gravityScale = gravityScale;
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        float movementAxis = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(movementAxis * speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetAxis("Jump") == 1f && onGround)
         {
-            gameObject.transform.position = new Vector2(gameObject.transform.position.x + speed * Time.deltaTime, gameObject.transform.position.y);
+            Jump();
+            doubleJumpTimer = 0f;
         }
-        if (Input.GetKey(KeyCode.A))
+
+        if (!onGround)
         {
-            gameObject.transform.position = new Vector2(gameObject.transform.position.x - speed * Time.deltaTime, gameObject.transform.position.y);
+            if (canDoubleJump)
+            {
+                doubleJumpTimer += Time.deltaTime;
+                if (doubleJumpTimer >= timeBetweenJumps)
+                {
+                    if (Input.GetAxis("Jump") == 1f)
+                    {
+                        Jump();
+                        canDoubleJump = false;
+                    }
+                }
+            }
+
+            if (Input.GetAxis("Jump") == -1f)
+            {
+                rb.gravityScale = fastFallGravityScale;
+            }
+            else
+            {
+                rb.gravityScale = gravityScale;
+            }
+        }
+
+        
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpHeight));
+        onGround = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+            canDoubleJump = true;
         }
     }
 }
