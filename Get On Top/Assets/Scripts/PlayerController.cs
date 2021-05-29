66356e0fb0cdc6ea75d203b8d5e606176928c12b
TomDotScott <tom.scott45456@gmail.com>
2021-05-29 13:51:47 +0100

    Enum for player state
    To make the player a bit more modular, an enum was introduced for their state

--- Get On Top/Assets/Scripts/PlayerController.cs
+++ Get On Top/Assets/Scripts/PlayerController.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    enum PlayerState
+    {
+        walking,
+        jumping,
+        dashing,
+        dead
+    }
+
+    private PlayerState playerState;
+
     [SerializeField] private float speed;
     [SerializeField] private float jumpHeight;
 
     private Rigidbody2D rb;
 
-    private bool onGround = false;
     private bool canDoubleJump = false;
 
     private float doubleJumpTimer;
@@ -29,51 +38,53 @@ public class PlayerController : MonoBehaviour
         float movementAxis = Input.GetAxis("Horizontal");
         transform.position = transform.position + new Vector3(movementAxis * speed * Time.deltaTime, 0, 0);
 
-        if (Input.GetAxis("Jump") == 1f && onGround)
-        {
-            Jump();
-            doubleJumpTimer = 0f;
-        }
 
-        if (!onGround)
+        switch (playerState)
         {
-            if (canDoubleJump)
-            {
-                doubleJumpTimer += Time.deltaTime;
-                if (doubleJumpTimer >= timeBetweenJumps)
+            case PlayerState.walking:
+                if (Input.GetAxis("Jump") == 1f)
                 {
-                    if (Input.GetAxis("Jump") == 1f)
+                    Jump();
+                    doubleJumpTimer = 0f;
+                    playerState = PlayerState.jumping;
+                }
+                break;
+            case PlayerState.jumping:
+                if (canDoubleJump)
+                {
+                    doubleJumpTimer += Time.deltaTime;
+                    if (doubleJumpTimer >= timeBetweenJumps)
                     {
-                        Jump();
-                        canDoubleJump = false;
+                        if (Input.GetAxis("Jump") == 1f)
+                        {
+                            Jump();
+                            canDoubleJump = false;
+                        }
                     }
                 }
-            }
 
-            if (Input.GetAxis("Jump") == -1f)
-            {
-                rb.gravityScale = fastFallGravityScale;
-            }
-            else
-            {
-                rb.gravityScale = gravityScale;
-            }
+                if (Input.GetAxis("Jump") == -1f)
+                {
+                    rb.gravityScale = fastFallGravityScale;
+                }
+                else
+                {
+                    rb.gravityScale = gravityScale;
+                }
+                break;
         }
-
-        
     }
 
     private void Jump()
     {
         rb.AddForce(new Vector2(0, jumpHeight));
-        onGround = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        if (collision.gameObject.CompareTag("Ground"))
         {
-            onGround = true;
+            playerState = PlayerState.walking;
             canDoubleJump = true;
         }
     }
